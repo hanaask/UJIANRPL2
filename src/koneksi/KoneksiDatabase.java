b8e91be4960d62da3ff3ad351c743e3507a5c10b
hanaask <hanakanthi@gmail.com>
2024-12-03 09:03:29 +0700

    uploud folder

---
+++ src/koneksi/KoneksiDatabase.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+/**
+ *
+ * @author hanak
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class KoneksiDatabase {
+    Connection cn;
+    public static Connection Koneksi(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/projectrpl2", "root", "");
+            return cn;
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+                return null;
+        }
+    }
+}
